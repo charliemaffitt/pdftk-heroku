9d6ce62475bbdcb87cca1f5aa55307cab66cf549
Charlie Maffitt <cmaffitt@highgroove.com>
2013-05-30 16:36:44 -0400

    Relocate binary files

--- lib/pdftk-heroku.rb
+++ lib/pdftk-heroku.rb
@@ -3,7 +3,9 @@ require "pdftk-heroku/version"
 module Pdftk
   module Heroku
 
-    PDFTK_PATH = File.expand_path "../../bin/pdftk", __FILE__
+    ENV['PATH'] = File.expand_path("../pdftk-heroku/binaries", __FILE__) + ":#{ENV['PATH']}"
+
+    ENV['LD_LIBRARY_PATH'] = File.expand_path("../pdftk-heroku/binaries", __FILE__) + ":#{ENV['LD_LIBRARY_PATH']}"
 
   end
 end
